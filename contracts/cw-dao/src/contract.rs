02d384fc4c6ed3437900209e62ca3fb78d16e83f
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2024-10-25 17:02:26 +0400

    edit cw-dao contract to add state

--- contracts/cw-dao/src/contract.rs
+++ contracts/cw-dao/src/contract.rs
@@ -1,24 +1,22 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
-// use cw2::set_contract_version;
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
-/*
-// version info for migration info
 const CONTRACT_NAME: &str = "crates.io:cw-dao";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
-*/
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
-    _deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
-    _msg: InstantiateMsg,
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
+    cw2::set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+    
     Ok(Response::default())
 }
 
--- contracts/cw-dao/src/state.rs
+++ contracts/cw-dao/src/state.rs
@@ -1,1 +1,46 @@
+use cosmwasm_schema::cw_serde;
+use cosmwasm_std::Uint128;
+use cw_storage_plus::{Item, Map};
 
+#[cw_serde]
+pub enum DAOCategory {
+    RealEstate,
+    Retail,
+    Hospitality,
+    Other,
+}
+
+#[cw_serde]
+pub struct DAOMetadata {
+    pub name: String,
+    pub symbol: String,
+    pub description: String,
+    pub image_uri: String,
+    pub category: DAOCategory,
+    pub category_other: Option<String>,
+}
+
+pub const DAO_METADATA: Item<DAOMetadata> = Item::new("dao_metadata");
+
+#[cw_serde]
+pub struct DAOStats {
+    pub num_properties: u64,
+    pub portfolio_value: u128,
+    pub num_investors: u64,
+    pub apy: u64,
+}
+
+pub const DAO_STATS: Item<DAOStats> = Item::new("dao_stats");
+
+#[cw_serde]
+pub struct DAOProperty {
+    pub price_per_share: Uint128,
+    pub estimated_monthly_income: Uint128,
+    pub estimated_apy: u64,
+    pub total_shares: u64,
+    pub status: String,
+    pub subcategory: String,
+    pub image_uri: String,
+}
+
+pub const DAO_PROPERTIES: Map<String, DAOProperty> = Map::new("dao_properties");

a6e24d2e410558c2e779f22560c29e6034c73220
FaisalAl-Tameemi <faisal.al.tameemi@gmail.com>
2024-10-26 22:28:58 +0400

    add property details query to cw-property

--- contracts/cw-property/src/query.rs
+++ contracts/cw-property/src/query.rs
@@ -1,6 +1,6 @@
 use cosmwasm_std::{Addr, Deps, Env, Response, StdResult, Uint128};
 use cw404::state::{BALANCES, TOTAL_SUPPLY};
-use crate::{msg::OutstandingSharesResponse};
+use crate::{msg::OutstandingSharesResponse, state::{Config, CONFIG}};
 
 pub fn query_outstanding_shares(deps: Deps, env: Env) -> StdResult<OutstandingSharesResponse> {
     let non_issued_shares = BALANCES.load(deps.storage, &env.contract.address)?;
@@ -16,4 +16,8 @@ pub fn query_outstanding_shares(deps: Deps, env: Env) -> StdResult<OutstandingSh
 
 pub fn query_share_balance(deps: Deps, _env: Env, address: Addr) -> StdResult<Uint128> {
     Ok(BALANCES.may_load(deps.storage, &address)?.unwrap_or_default())
+}
+
+pub fn query_property_details(deps: Deps, _env: Env) -> StdResult<Config> {
+    Ok(CONFIG.load(deps.storage)?)
 }

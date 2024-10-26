57a18795157ed71ecd50ea8bb8f2d48aa1c0bc13
wanesurf <helwan.mande.1@ens.etsmtl.ca>
2024-10-26 19:50:27 +0400

    feat: improve ui

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -194,15 +194,15 @@ export default function Example() {
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Get started
+                Create a Trust
               </a>
               <a
                 href="#"
                 className="text-sm font-semibold leading-6 text-gray-900"
               >
-                Learn more <span aria-hidden="true">→</span>
+                List an asset <span aria-hidden="true">→</span>
               </a>
             </div>
           </div>
